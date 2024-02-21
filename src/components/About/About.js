8730c4f6f258d32de45da1392632835a47593134
Karmanya <karmanyasingh8@gmail.com>
2024-02-21 23:00:00 +0530

    about color

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -35,7 +35,7 @@ function About() {
           </Col>
         </Row>
         <h1 className="project-heading">
-          Professional <strong className="purple">Skillset </strong>
+          Professional Skillset
         </h1>
 
         <Techstack />
