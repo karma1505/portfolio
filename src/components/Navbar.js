9f6f4806efb1058f93a2f4aed6ea39661bfd373a
Karmanya <karmanyasingh8@gmail.com>
2024-03-01 10:52:18 +0530

    revert

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -48,7 +48,7 @@ function NavBar() {
           <Nav className="ms-auto" defaultActiveKey="#home">
             <Nav.Item>
               <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
-                <AiOutlineHome style={{ marginBottom: "2px" }} /> Homer
+                <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
               </Nav.Link>
             </Nav.Item>
 
